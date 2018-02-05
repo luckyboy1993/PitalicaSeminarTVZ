dc2792d45722189262caa5e4f42dc7f33ff30449
Lucian Oslakovic <lucian.oslakovic@gmail.com>
2018-02-05 14:43:21 +0100

    Add project files.

---
+++ PitalicaSeminar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PitalicaSeminar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
